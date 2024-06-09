b32d3de5fcc416dcebde124916e3d1b62bb796db
Mukul Nayak <mukulnayak2004@gmail.com>
2024-06-09 19:02:21 +0530

    Controllers adder

---
+++ controllers/postController.js
@@ -0,0 +1,36 @@
+const Post=require("../models/postModel");
+
+exports.createPost=async (req,res)=>{
+    try {
+        const {title,body}=req.body;
+        const post=new Post({
+            title,body
+        });
+        const savedPost=await post.save();
+
+        res.json({
+            post:savedPost
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            error:"Error while creating Post"
+        })
+    }
+}
+
+exports.getAllPosts=async (req,res)=>{
+    try {
+        // const posts=await Post.find().populate("likes").populate("comments").exec();
+        const posts=await Post.find()
+        res.json({
+            posts
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            error:"Error while creating Post"
+        })
+    }
+}
